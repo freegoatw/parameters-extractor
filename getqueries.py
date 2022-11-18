163410eb9edbd2d34599a2887b2a1b3b7429a40d
Dabu <mr.loska@gmail.com>
2022-11-18 14:31:20 +0100

    stable

--- getqueries.py
+++ getqueries.py
@@ -1,13 +1,31 @@
-#list the most used parameters in a url list
+#This script lists the most used parameters in a url list
 from urllib.parse import urlparse
+from pathlib import Path
+from detect_delimiter import detect
+
+scriptError = False
 
 corpuslist = [];
 
-with open("urls.txt", 'r') as f:
-    corpus = f.read().split(",");
+workingDirectory = str(Path(__file__).parent.absolute())
+workingDirectory = workingDirectory[-20:]
+filePath = str(Path().absolute())
+filePath = filePath[-20:]
+
+if workingDirectory != filePath : print("attention ton terminal n’est pas au bon endroit, le working directory est : " + workingDirectory +"\n et le filepath est : " + filePath)
+else : print("zeee partii")
+
+#Open the url file and splits it for each ",""
+try :
+    with open("urls.txt", 'r') as f:
+        corpus = f.read().split("\n");
+except :
+    print("il y a un problème avec le fichier des urls")
+    scriptError = True
   
 query_counter = {};  
 
+#Main function of the script. get the parameters.
 def get_query(url):
     url = url
     query = urlparse(url).query;
@@ -16,20 +34,25 @@ def get_query(url):
     query = list(set(query));
     return query;
 
-#count the number of times a query is used           
-for url in corpus:
-    query = get_query(url);
-    corpuslist.append(query);
-    for q in query:
-        if q in query_counter:
-            query_counter[q] += 1;
-        else:
-            query_counter[q] = 1;
+#Check if there is a problem before
+if scriptError == False :
 
-
-
-# csv export one query per line
-with open("queries.csv", 'w') as f:
-    for query in corpuslist:
+    #count the number of times a parameter is used.
+    for url in corpus:
+        query = get_query(url);
+        corpuslist.append(query);
         for q in query:
-            f.write(q + "\n");
+            if q in query_counter:
+                query_counter[q] += 1;
+            else:
+                query_counter[q] = 1;
+
+    # csv export one query per line.
+    with open("queries.csv", 'w') as f:
+        for query in corpuslist:
+            for q in query:
+                f.write(q + "\n");
+
+    print("\n\n\n##################################### \n   ding ding - votre csv est prêt \n#####################################\n\n\n")
+
+else : print("\n\n\n_.~\"~._.~\"~._.~\"~._.~\"~._ Débug nécéssaire frr_.~\"~._.~\"~._.~\"~._.~\"~._\n\n\n")
